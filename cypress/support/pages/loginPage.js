e7113199b7dfed8eb6f3768c5cdc578b85feed1f
Beatriz Barbosa Costa Serra <beatrizbcserra@gmail.com>
2026-02-10 14:54:00 -0300

    Add files via upload

---
+++ cypress/support/pages/loginPage.js
@@ -0,0 +1,36 @@
+class LoginPage {
+  visitar() {
+    cy.visit('https://carlosfelixpenha-create.github.io/QAPlayground')
+    cy.contains('Login').click() 
+  }
+
+  preencherEmail(email) {
+    cy.get('input[placeholder="Digite seu e-mail"]').clear().type(email)
+  }  
+
+  preencherSenha(senha) {
+    cy.get('input[placeholder="Digite sua senha"]').clear().type(senha) 
+  }
+
+  submeter() {
+    cy.contains('Entrar').click()
+  }
+
+  validarLoginSucesso(mensagem) {
+    cy.get('.modal-content')
+      .should('be.visible')
+      .and('contain', mensagem)
+    cy.contains('OK').click()
+  }
+
+  preencherLogin(user) {
+  this.preencherEmail(user.email)
+  this.preencherSenha(user.senha)
+  cy.get('input[type="checkbox"]').check() 
+  
+  this.submeter()
+  }
+
+}
+
+export default new LoginPage()
